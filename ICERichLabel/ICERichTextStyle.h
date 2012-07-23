d8605e9faa1474780d572a0c7249e334e0cc064d
Icepat <patrick.trillsam@gmail.com>
2012-07-23 14:33:35 +0800

    First commit.

---
+++ ICERichLabel/ICERichTextStyle.h
@@ -0,0 +1,51 @@
+//
+//  ICERichTextStyle.h
+//  ICEDK
+//
+//  Created by Icepat on 27/04/12.
+//  Copyright (c) 2012 Icepat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICERichTextStyle : NSObject {
+    // Define a text style, based on font, color...
+	UIFont *font_;
+	UIColor *color_;	
+    UIColor *backgroundColor_;
+    NSString *prefix_;
+    
+    // Hidden prefix when on screen.
+    BOOL prefixIsHidden_;
+    
+    // Target & action.
+	id __unsafe_unretained target_;
+	SEL action_;
+}
+
+@property (nonatomic) UIFont *font;
+@property (nonatomic) UIColor *color;
+@property (nonatomic) UIColor *backgroundColor;
+@property (nonatomic) NSString *prefix;
+@property (nonatomic, assign) BOOL prefixIsHidden;
+@property (unsafe_unretained, nonatomic, readonly) id target;
+@property (nonatomic, readonly) SEL action;
+
+// Define the style.
++ (ICERichTextStyle*)styleWithFont:(UIFont*)font
+                             color:(UIColor*)color
+                         backColor:(UIColor*)backgroundColor
+                            prefix:(NSString*)prefix
+                        hidePrefix:(BOOL)enabled;
+// Define the style with the target and action.
++ (ICERichTextStyle*)styleWithFont:(UIFont*)font
+                             color:(UIColor*)color
+                         backColor:(UIColor*)backgroundColor
+                            prefix:(NSString*)prefix
+                        hidePrefix:(BOOL)enabled
+                            target:(id)target
+                         andAction:(SEL)action;
+// Set or change the target and action.
+- (void)addTarget:(id)target action:(SEL)action;
+
+@end
---
+++ ICETapLabel/ICETapLabel.m
@@ -0,0 +1,115 @@
+//
+//  ICETapLabel.m
+//  ICEDK
+//
+//  Created by Icepat on 19/04/12.
+//  Copyright (c) 2012 Icepat. All rights reserved.
+//
+
+#import "ICETapLabel.h"
+#import "ICEView+Utilities.h"
+
+@interface ICETapLabel()
+// Handle the Tap events.
+- (void)handleTouch;
+@end
+
+@implementation ICETapLabel
+@synthesize label = label_;
+@synthesize linkedObject = linkedObject_;
+
+#pragma mark - Init.
+// Init.
+- (id)init{
+    self = [super init];
+    if (self){
+        label_ = [[UILabel alloc] init];
+        [label_ setBackgroundColor:[UIColor clearColor]];
+    }
+    return self;
+}
+
+// Init with rect.
+- (id)initWithRect:(CGRect)rect{
+    self = [self init];
+    if (self){
+        [self setTheFrame:rect];
+    }
+    return self;
+}
+
+// Init with coder.
+- (id)initWithCoder:(NSCoder*)coder{
+    if ((self = [super initWithCoder:coder])) {
+        label_ = [[UILabel alloc] init];
+        [label_ setBackgroundColor:[UIColor clearColor]];
+        [self setTheFrame:self.frame];        
+	}
+	return self;
+}
+
+
+#pragma mark - Tap on label handler.
+// Set the self and UILabel frame.
+- (void)setTheFrame:(CGRect)frame{
+    [self removeSubviews];
+    
+    // UILabel is on the entire view.
+    [label_ setFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+    
+    self.frame = frame;
+    [self addSubview:label_];
+}
+
+#pragma mark - Target/action/touch management.
+// Set target and action.
+- (void)addTarget:(id)target action:(SEL)action{
+    target_ = target;
+    action_ = action;
+    
+    // Init tap gesture recognizer.
+    UITapGestureRecognizer *touchGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTouch)];
+    [self addGestureRecognizer:touchGesture];
+
+    // Clean.
+}
+
+// Handle the Tap events.
+- (void)handleTouch{
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    [target_ performSelector:action_ withObject:self];
+    #pragma clang diagnostic pop
+}
+
+#pragma mark - UILabel compatibility.
+// Get the text.
+- (NSString*)text{
+    return [label_ text];
+}
+
+// Set the text.
+- (void)setText:(NSString*)text{
+    [label_ setText:text];
+    
+    // UILabel is on the entire view.
+    [label_ setFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];    
+}
+
+// Set the text color.
+- (void)setTextColor:(UIColor*)textColor{
+    [label_ setTextColor:textColor];
+}
+
+// Set the background color.
+- (void)setBackgroundColor:(UIColor*)backgroundColor{
+    [super setBackgroundColor:backgroundColor];
+    [label_ setBackgroundColor:backgroundColor];
+}
+
+// Set the label font.
+- (void)setFont:(UIFont*)font{
+    [label_ setFont:font];
+}
+
+@end
